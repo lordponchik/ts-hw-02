9a5fde5f1d9f18fc3ba7f7423045c6b425510916
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-07 17:59:05 +0200

    add structure for projekt

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/ts-hw-02/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        page1: 'src/html/task-01.html',
+        page2: 'src/html/task-02.html',
+        page3: 'src/html/task-03.html',
+        page4: 'src/html/task-04.html',
+        page5: 'src/html/task-05.html',
+        page6: 'src/html/task-06.html',
+      },
+    },
+  },
+});
