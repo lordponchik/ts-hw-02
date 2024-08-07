e95a38b83c44abdbd87e1c97d1785de82f3771e2
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-07 18:19:47 +0200

    add task-01

--- src/scripts/task-01.ts
+++ src/scripts/task-01.ts
@@ -1,1 +1,9 @@
-export {};
+const logItems = function <T>(array: T[]): void {
+  for (let i: number = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+};
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+console.log('');
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
