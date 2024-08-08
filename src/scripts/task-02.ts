0baaf3a1054b633243c71334e1ea3696a3ebd83c
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-08 21:29:35 +0200

    add task-02

--- src/scripts/task-02.ts
+++ src/scripts/task-02.ts
@@ -1,1 +1,10 @@
+console.log('task-02');
+
+const calculateEngravingPrice = function (message: string, pricePerWord: number): number {
+  return message.split(' ').length * pricePerWord;
+};
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); //240
+
 export {};
