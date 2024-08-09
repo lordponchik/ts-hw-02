1a6a617ed599e0cd9dc852fdfbbe963e81b831d0
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-09 17:51:42 +0200

    add task-06

--- src/scripts/task-06.ts
+++ src/scripts/task-06.ts
@@ -1,1 +1,24 @@
+console.log('task-06');
+
+let input: string | null;
+const numbers: number[] = [];
+let total: number = 0;
+
+do {
+  input = prompt('Введите число');
+
+  if (input !== null)
+    if (isNaN(Number(input))) {
+      alert('Было введено не число, попробуйте еще раз');
+    } else {
+      numbers.push(Number(input));
+    }
+} while (input !== null);
+
+for (const value of numbers) {
+  total += value;
+}
+
+console.log(total);
+
 export {};
