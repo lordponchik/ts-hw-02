33fbcc436b58549d8335dd119e0c76bfce1e63a6
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-09 16:44:35 +0200

    add task-04

--- src/scripts/task-04.ts
+++ src/scripts/task-04.ts
@@ -1,1 +1,17 @@
+const formatString = function (string: string) {
+  return string.length > 40 ? `${string.slice(0, 40)}...` : string;
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+// вернется форматированная строка
+
 export {};
