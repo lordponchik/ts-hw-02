12d637dc805aeb0d44e7148abf973737769589ac
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-09 17:14:27 +0200

    add task-05

--- src/scripts/task-05.ts
+++ src/scripts/task-05.ts
@@ -1,1 +1,15 @@
+console.log('task-05');
+
+const checkForSpam = function (message: string): boolean {
+  return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
 export {};
